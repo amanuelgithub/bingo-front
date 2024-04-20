57fee1f6ad98e23050b0bd30617e2374c08c5656
amanuelgithub <amanuelgirma070@gmail.com>
2024-04-20 11:13:13 +0300

    chagned the remote base url to new server address

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -1,5 +1,5 @@
 export const APP_NAME = "Virtual Bingo";
 // export const REMOTE_BASE_URL = "https://bingo-v8hl.onrender.com/api";
 // export const REMOTE_BASE_URL = "http://157.230.187.85:5000/api";
-export const REMOTE_BASE_URL = "http://157.230.187.85:5000/api";
-export const BASE_URL = "http://localhost:3002/api";
+export const REMOTE_BASE_URL = "http://157.230.49.88:5000/api";
+export const BASE_URL = "http://localhost:5000/api";
