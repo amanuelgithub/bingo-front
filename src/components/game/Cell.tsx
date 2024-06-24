190399c4b5679a6b2e87be6582a43cd77d6b0739
amanuelgithub <amanuelgirma070@gmail.com>
2024-06-24 19:38:58 +0300

    update: changed cell background & text color.

--- src/components/game/Cell.tsx
+++ src/components/game/Cell.tsx
@@ -9,12 +9,8 @@ interface Props {
 
 function Cell({ called, isInCalled, ballNumber, index }: Props) {
   return (
-    <div
-      className={`${
-        !called || isInCalled ? "hidden" : "block"
-      } relative h-[70%] w-[6%]`}
-    >
-      <div className="flex h-[100%] w-[100%] items-center justify-center rounded-[100%] bg-purple-700 bg-opacity-50 text-white">
+    <div className={`${!called || isInCalled ? "hidden" : "block"} relative h-[70%] w-[6%]`}>
+      <div className="flex h-[100%] w-[100%] items-center justify-center rounded-[100%] bg-gray-500 bg-opacity-50 text-gray-200">
         <span className="font-semibold" style={{ fontSize: "1.75vw" }}>
           {ballNumber}
         </span>
